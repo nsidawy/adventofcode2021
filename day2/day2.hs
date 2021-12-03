d8ccff01f8a0601f4d3742142b6b1f78a1c8ba65
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-03 16:53:36 -0500

    day2 answer

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,27 +1,45 @@
 import System.IO  
 import Control.Monad
+import Data.List.Split
+import Data.Tuple.Select
+
+data Movement = Up | Forward | Down
 
 main = do  
-    list <- getNumbers
-    print $ getIncreases list
-    print $ getIncreases2 list
+    lines <- getLines "input.txt"
+    let directions = map processLine lines
+    let result = navigate directions (0, 0)
+    let result2 = navigate2 directions (0, 0, 0)
+    print result
+    print $ (fst result) * (snd result)
+    print result2
+    print $ (sel1 result2) * (sel2 result2)
 
-getNumbers :: IO [Int]
-getNumbers = do  
-        let list = []
-        handle <- openFile "input.txt" ReadMode
-        contents <- hGetContents handle
-        let singlewords = words contents
-            list = toInt singlewords
-        return list
+navigate :: [(String, Int)] -> (Int, Int) -> (Int, Int)
+navigate [] (p, d) = (p, d)
+navigate (("forward", n) : ds) (p, d) = navigate ds (p + n, d)
+navigate (("down", n) : ds) (p, d) = navigate ds (p, d + n)
+navigate (("up", n) : ds) (p, d) = navigate ds (p, d - n)
+navigate _ _ = (0,0)
 
-getIncreases :: [Int] -> Int
-getIncreases (x:y:xs) = (if x < y then 1 else 0) + getIncreases (y : xs) 
-getIncreases _ = 0
+navigate2 :: [(String, Int)] -> (Int, Int, Int) -> (Int, Int, Int)
+navigate2 [] (p, d, a) = (p, d, a)
+navigate2 (("forward", n) : ds) (p, d, a) = navigate2 ds (p + n, d + a * n, a)
+navigate2 (("down", n) : ds) (p, d, a) = navigate2 ds (p, d, a + n)
+navigate2 (("up", n) : ds) (p, d, a) = navigate2 ds (p, d, a - n)
+navigate2 _ _ = (0,0,0)
 
-getIncreases2 :: [Int] -> Int
-getIncreases2 (v1:v2:v3:v4:xs) = (if v1 + v2 + v3 < v2 + v3 + v4 then 1 else 0) + getIncreases2 (v2 : v3 : v4 : xs) 
-getIncreases2 _ = 0
+getLines :: String -> IO [String]
+getLines path = lines <$> readFile path
 
-toInt :: [String] -> [Int]
-toInt = map read
+processLine :: String -> (String, Int)
+processLine line = (head values, read $ last values)
+    where
+        values = splitOn " " line
+
+parseMovement :: String -> Movement
+parseMovement s = case s of 
+    "forward" -> Forward
+    "up" -> Up
+    "down" -> Down
+    _ -> error "invalid movement"
