f8d9bd0821b5660d3a980a779f36b8ffdba4f567
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-03 21:46:33 -0500

    day 3

---
+++ day3/day3.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE ParallelListComp #-}
+
+import Control.Monad
+import Data.Bits
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+main = do  
+    lines <- getLines "input.txt"
+    let counts = replicate (length $ head lines) 0
+    let gammaBinary = getGammaBinary lines counts
+    let epsilonBinary = getEpsilonBinary gammaBinary
+    print $ (binaryToInt gammaBinary) * (binaryToInt epsilonBinary) 
+    let oxygenBinary = getOxygenBinary lines 0
+    let co2Binary = getCo2Binary lines 0
+    print $ (binaryToInt $ co2Binary) * (binaryToInt $ oxygenBinary)
+
+getOxygenBinary :: [String] -> Int -> [Int]
+getOxygenBinary [s] _ = [read [c] | c <- s]
+getOxygenBinary ls i = getOxygenBinary (filter f ls) (i+1)
+    where 
+        g :: [Int]
+        g = getGammaBinary ls (replicate (length $ head ls) 0)
+    
+        f :: String -> Bool
+        f l = read [(l !! i)] == g !! i
+
+getCo2Binary :: [String] -> Int -> [Int]
+getCo2Binary [s] _ = [read [c] | c <- s]
+getCo2Binary ls i = getCo2Binary (filter f ls) (i+1)
+    where 
+        e :: [Int]
+        e = getEpsilonBinary $ getGammaBinary ls (replicate (length $ head ls) 0)
+    
+        f :: String -> Bool
+        f l = read [(l !! i)] == e !! i
+
+getEpsilonBinary :: [Int] -> [Int]
+getEpsilonBinary gammaBinary = [if b == 0 then 1 else 0 | b <- gammaBinary]
+
+getGammaBinary :: [String] -> [Int] -> [Int]
+getGammaBinary [] counts = [if c < 0 then 0 else 1 | c <- counts]
+getGammaBinary (l:ls) counts = getGammaBinary ls newCounts
+    where
+        changes :: [Int]
+        changes = getChanges l []
+        newCounts :: [Int]
+        newCounts = applyChanges counts changes
+
+getChanges :: [Char] -> [Int] -> [Int]
+getChanges [] i = i
+getChanges (c:cs) i = getChanges cs $ i ++ [if c == '0' then -1 else 1]
+
+applyChanges :: [Int] -> [Int] -> [Int]
+applyChanges counts changes = [i + c | i <- counts | c <- changes]
+
+getLines :: String -> IO [String]
+getLines path = lines <$> readFile path
+
+binaryToInt :: [Int] -> Int
+binaryToInt bs = sum [b * 2^i | (b, i) <- zip bs indices]
+    where 
+        indices :: [Int]
+        indices = reverse [0..(length bs) - 1]
