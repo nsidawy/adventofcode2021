3b7d74ecd996fbeb0952f4f000a9a2370fbe193d
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-04 16:38:41 -0500

    cleaner day4

--- day4/day4.hs
+++ day4/day4.hs
@@ -3,6 +3,7 @@
 import Control.Monad
 import Text.Printf
 import Data.List.Split
+import Data.Sort
 import qualified Data.Vector as V
 
 type Board = V.Vector (V.Vector Int)
@@ -10,10 +11,11 @@ type Row = V.Vector Int
 
 main = do  
     (numbers, boards) <- getData "input.txt"
-    let winners = getWinners numbers 1 boards []
-    print winners
-    printf "Part 1: %d\n" $ scoreBoard numbers $ head winners
-    printf "Part 2: %d\n" $ scoreBoard numbers $ last winners
+    let winningTurns = sortOn (\v -> snd v) [(b, getWinningTurn numbers 1 b) | b <- boards]
+    let (firstBoard, firstTurn) = head winningTurns
+    let (lastBoard, lastTurn) = last winningTurns
+    printf "Part 1: %d\n" $ scoreBoard (firstTurn `take` numbers) firstBoard
+    printf "Part 2: %d\n" $ scoreBoard (lastTurn `take` numbers) lastBoard
 
 getData :: String -> IO ([Int], [Board])
 getData path = do
@@ -22,17 +24,8 @@ getData path = do
     let boards = getBoards (tail lines) []
     return (numbers, boards)
 
-getWinners :: [Int] -> Int -> [Board] -> [(Int, Board)] -> [(Int, Board)]
-getWinners ns i boards winners = if length boards == 0 then winners else getWinners ns (i+1) losers (winners ++ newWinners)
-    where
-        called :: [Int]
-        called = take i ns
-
-        losers :: [Board]
-        losers = [b | b <- boards, not $ checkBoard called b]
-        
-        newWinners :: [(Int, Board)]
-        newWinners = zip (repeat i) [b | b <- boards, checkBoard called b]
+getWinningTurn :: [Int] -> Int -> Board -> Int
+getWinningTurn ns i board = if checkBoard (take i ns) board then i else getWinningTurn ns (i+1) board
 
 getBoards :: [String] -> [Board] -> [Board]
 getBoards [] v = v
@@ -44,15 +37,11 @@ getBoards ls v = getBoards (drop 6 ls) (newBoard : v)
         newBoard :: Board
         newBoard = V.fromList [V.fromList ((map read $ words bl) :: [Int]) | bl <- boardLines] 
 
-scoreBoard :: [Int] -> (Int, Board) -> Int
-scoreBoard ns (i, board) = (ns !! (i-1)) * (sum [v | v <- flattened, not $ v `elem` called])
+scoreBoard :: [Int] -> Board -> Int
+scoreBoard ns board = (last ns) * (sum [v | v <- flattened, not $ v `elem` ns])
     where
         flattened :: [Int]
         flattened = V.toList $ V.concat $ V.toList board
-
-        called :: [Int]
-        called = i `take` ns
-        
 
 checkBoard :: [Int] -> Board -> Bool
 checkBoard ns board = validRows || validCols
