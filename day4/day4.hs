61d97d949243eeb94af5adc1b1fa1227b29533a1
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-04 10:46:31 -0500

    day4 part 2

--- day4/day4.hs
+++ day4/day4.hs
@@ -10,8 +10,10 @@ type Row = V.Vector Int
 
 main = do  
     (numbers, boards) <- getData "input.txt"
-    let (called, winner) = getWinner numbers 1 boards
-    printf "Part 1: %d\n" $ scoreBoard called winner
+    let winners = getWinners numbers 1 boards []
+    print winners
+    printf "Part 1: %d\n" $ scoreBoard numbers $ head winners
+    printf "Part 2: %d\n" $ scoreBoard numbers $ last winners
 
 getData :: String -> IO ([Int], [Board])
 getData path = do
@@ -20,14 +22,17 @@ getData path = do
     let boards = getBoards (tail lines) []
     return (numbers, boards)
 
-getWinner :: [Int] -> Int -> [Board] -> ([Int], Board)
-getWinner ns i boards = if length winners > 0 then (called, head winners) else getWinner ns (i+1) boards
+getWinners :: [Int] -> Int -> [Board] -> [(Int, Board)] -> [(Int, Board)]
+getWinners ns i boards winners = if length boards == 0 then winners else getWinners ns (i+1) losers (winners ++ newWinners)
     where
         called :: [Int]
         called = take i ns
 
-        winners :: [Board]
-        winners = [b | b <- boards, checkBoard called b]
+        losers :: [Board]
+        losers = [b | b <- boards, not $ checkBoard called b]
+        
+        newWinners :: [(Int, Board)]
+        newWinners = zip (repeat i) [b | b <- boards, checkBoard called b]
 
 getBoards :: [String] -> [Board] -> [Board]
 getBoards [] v = v
@@ -39,11 +44,15 @@ getBoards ls v = getBoards (drop 6 ls) (newBoard : v)
         newBoard :: Board
         newBoard = V.fromList [V.fromList ((map read $ words bl) :: [Int]) | bl <- boardLines] 
 
-scoreBoard :: [Int] -> Board -> Int
-scoreBoard ns board = (last ns) * (sum [v | v <- flattened, not $ v `elem` ns])
+scoreBoard :: [Int] -> (Int, Board) -> Int
+scoreBoard ns (i, board) = (ns !! (i-1)) * (sum [v | v <- flattened, not $ v `elem` called])
     where
         flattened :: [Int]
         flattened = V.toList $ V.concat $ V.toList board
+
+        called :: [Int]
+        called = i `take` ns
+        
 
 checkBoard :: [Int] -> Board -> Bool
 checkBoard ns board = validRows || validCols
