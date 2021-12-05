64735b2faf2d8bb479d9a0370d3905d618a5eb54
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-05 01:57:53 -0500

    day 5 gross

--- day5/day5.hs
+++ day5/day5.hs
@@ -4,57 +4,59 @@ import Data.List.Split
 import Data.Sort
 import qualified Data.Vector as V
 
-type Board = V.Vector (V.Vector Int)
-type Row = V.Vector Int
+type Point = (Int,Int)
+type Coord = (Point,Point)
 
 main = do  
-    (numbers, boards) <- getData "input.txt"
-    let winningTurns = sortOn snd [(b, getWinningTurn numbers 1 b) | b <- boards]
-    let (firstBoard, firstTurn) = head winningTurns
-    let (lastBoard, lastTurn) = last winningTurns
-    printf "Part 1: %d\n" $ scoreBoard (firstTurn `take` numbers) firstBoard
-    printf "Part 2: %d\n" $ scoreBoard (lastTurn `take` numbers) lastBoard
+    coords <- getData "input.txt"
+    let straightCoords = filter isStraight coords
+    let maxX = maximum $ concatMap (\i -> [fst $ fst i, fst $ snd i]) coords
+    let maxY = maximum $ concatMap (\i -> [snd $ fst i, snd $ snd i]) coords
+    let points = [(x,y) | x <- [0..maxX] , y <- [0..maxY]]
+    let hotSpots1 = [p | p <- points, countIntersects straightCoords p >= 2]
+    print (length hotSpots1)
+    let hotSpots2 = [p | p <- points, countIntersects coords p >= 2]
+    print (length hotSpots2)
 
-getData :: String -> IO ([Int], [Board])
+countIntersects :: [Coord] -> Point -> Int
+countIntersects cs p = length (filter (intersects p) cs)
+
+intersects :: Point -> Coord -> Bool
+intersects p c = straightIntersects p c && (isStraight c || diffX == diffY)
+    where
+        diffX :: Int
+        diffX = abs $ fst p - firstX c
+        diffY :: Int
+        diffY = abs $ snd p - firstY c
+
+straightIntersects :: Point -> Coord -> Bool
+straightIntersects p c = ((fst p >= firstX c && fst p <= sndX c) || (fst p >= sndX c && fst p <= firstX c))
+        && ((snd p >= firstY c && snd p <= sndY c) || (snd p >= sndY c && snd p <= firstY c))
+
+diagonalIntersects :: Point -> Coord -> Bool
+diagonalIntersects p c = False
+
+getPoints :: Coord -> [Point]
+getPoints c = []
+        
+isStraight :: Coord -> Bool
+isStraight c = firstX c == sndX c || firstY c == sndY c
+
+firstX :: Coord -> Int
+firstX c = fst $ fst c
+
+sndX :: Coord -> Int
+sndX c = fst $ snd c
+
+firstY :: Coord -> Int
+firstY c = snd $ fst c
+
+sndY :: Coord -> Int
+sndY c = snd $ snd c
+
+getData :: String -> IO [Coord]
 getData path = do
     lines <- lines <$> readFile path
-    let numbers = map read $ splitOn "," $ head lines
-    let boards = getBoards $ tail lines
-    return (numbers, boards)
-
-getBoards :: [String] -> [Board]
-getBoards [] = []
-getBoards ls = newBoard : getBoards ( drop 6 ls)
-    where 
-        boardLines :: [String]
-        boardLines = take 5 $ tail ls 
-
-        newBoard :: Board
-        newBoard = V.fromList [V.fromList ((map read $ words bl) :: [Int]) | bl <- boardLines] 
-
-getWinningTurn :: [Int] -> Int -> Board -> Int
-getWinningTurn ns i board
-    | checkBoard (take i ns) board = i
-    | otherwise = getWinningTurn ns (i+1) board
-
-scoreBoard :: [Int] -> Board -> Int
-scoreBoard ns board = last ns * sum [v | v <- flattened, v `notElem` ns]
-    where
-        flattened :: [Int]
-        flattened = V.toList $ V.concat $ V.toList board
-
-checkBoard :: [Int] -> Board -> Bool
-checkBoard ns board = validRows || validCols
-    where 
-        checkRow :: [Int] -> Row -> Bool 
-        checkRow ns row = V.all (`elem` ns) row
-        
-        checkColumn :: [Int] -> Board -> Int -> Bool
-        checkColumn ns board i = all (`elem` ns) (V.map (V.! i) board)
-
-        validRows :: Bool
-        validRows = V.any (checkRow ns) board
-
-        validCols :: Bool
-        validCols = any (checkColumn ns board) [0..4]
-
+    let coordsStr = map (\l -> map (map read . splitOn "," ) $ splitOn " -> " l) lines
+    let coords = [((head $ head x, last $ head x), (head $ last x, last $ last x)) | x <- coordsStr]
+    return coords
