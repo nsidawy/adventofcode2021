aca405879916dc41c937c73e82187cd88c3b2871
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-05 12:54:07 -0500

    clean day5

--- day5/day5.hs
+++ day5/day5.hs
@@ -1,8 +1,7 @@
 import Control.Monad
 import Text.Printf
 import Data.List.Split
-import Data.Sort
-import qualified Data.Vector as V
+import qualified Data.Map as M
 
 type Point = (Int,Int)
 type Coord = (Point,Point)
@@ -10,55 +9,35 @@ type Coord = (Point,Point)
 main = do  
     coords <- getData "input.txt"
     let straightCoords = filter isStraight coords
-    let maxX = maximum $ concatMap (\i -> [fst $ fst i, fst $ snd i]) coords
-    let maxY = maximum $ concatMap (\i -> [snd $ fst i, snd $ snd i]) coords
-    let points = [(x,y) | x <- [0..maxX] , y <- [0..maxY]]
-    let hotSpots1 = [p | p <- points, countIntersects straightCoords p >= 2]
-    print (length hotSpots1)
-    let hotSpots2 = [p | p <- points, countIntersects coords p >= 2]
-    print (length hotSpots2)
+    let straightLinePoints = concat [uncurry getPoints c | c <- straightCoords]
+    let allLinePoints = concat [uncurry getPoints c | c <- coords]
+    let hotSpots1 = countHotSpots straightLinePoints
+    let hotSpots2 = countHotSpots allLinePoints
+    print hotSpots1
+    print hotSpots2
 
-countIntersects :: [Coord] -> Point -> Int
-countIntersects cs p = length (filter (intersects p) cs)
+countHotSpots :: [Point] -> Int
+countHotSpots ps = length $ filter (\ (_, c) -> c > 1) $ M.toList $ M.fromListWith (+) [(p, 1) | p <- ps]
 
-intersects :: Point -> Coord -> Bool
-intersects p c = bounded p c && (isStraight c || diffX == diffY)
-    where
-        diffX :: Int
-        diffX = abs $ fst p - fst (fst c)
-        diffY :: Int
-        diffY = abs $ snd p - snd (fst c)
+getPoints :: Point -> Point -> [Point]
+getPoints (x1, y1) (x2, y2)
+    | x1 == x2 && y1 == y2 = [(x1, y1)]
+    | x1 == x2 && y1 < y2 = (x1, y1) : getPoints (x1, y1 + 1) (x2, y2) 
+    | x1 == x2 && y1 > y2 = (x1, y1) : getPoints (x1, y1 - 1) (x2, y2) 
+    | x1 < x2 && y1 == y2 = (x1, y1) : getPoints (x1 + 1, y1) (x2, y2) 
+    | x1 > x2 && y1 == y2 = (x1, y1) : getPoints (x1 - 1, y1) (x2, y2) 
+    | otherwise = (x1, y1) : getPoints (x1 + diagXInc , y1 + diagYInc) (x2, y2)
+    where 
+        diagXInc :: Int
+        diagXInc = if x1 < x2 then 1 else -1 
+        diagYInc :: Int
+        diagYInc = if y1 < y2 then 1 else -1 
 
-bounded :: Point -> Coord -> Bool
-bounded p c = (fst p >= minX c && fst p <= maxX c)
-        && (snd p >= minY c && snd p <= maxY c)
-        
 isStraight :: Coord -> Bool
-isStraight c = fst (fst c) == fst (snd c) || snd (fst c) == snd (snd c)
-
-minX :: Coord -> Int
-minX c 
-    | fst (fst c) < fst (snd c) = fst $ fst c
-    | otherwise = fst $ snd c
-
-maxX :: Coord -> Int
-maxX c 
-    | fst (fst c) < fst (snd c) = fst $ snd c
-    | otherwise = fst $ fst c
-
-minY :: Coord -> Int
-minY c 
-    | snd (fst c) < snd (snd c) = snd $ fst c
-    | otherwise = snd $ snd c
-
-maxY :: Coord -> Int
-maxY c 
-    | snd (fst c) < snd (snd c) = snd $ snd c
-    | otherwise = snd $ fst c
+isStraight ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
 
 getData :: String -> IO [Coord]
 getData path = do
     lines <- lines <$> readFile path
-    let coordsStr = map (\l -> map (map read . splitOn "," ) $ splitOn " -> " l) lines
-    let coords = [((head $ head x, last $ head x), (head $ last x, last $ last x)) | x <- coordsStr]
-    return coords
+    let coordsStr = map (map (map read . splitOn "," ) . splitOn " -> ") lines
+    return [((head $ head c, last $ head c), (head $ last c, last $ last c)) | c <- coordsStr]
