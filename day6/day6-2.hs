54b16707988bb3906d3fe26ce92313b4e13d874d
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-06 01:41:41 -0500

    day6 clean up

--- day6/day6-2.hs
+++ day6/day6-2.hs
@@ -10,13 +10,16 @@ type Coord = (Point,Point)
 main = do  
     inputTimers <- getData "input.txt"
     let days = 128 
-    let (zeroCounts, dailyTimers) = run days [[0]] [1]
-    let result = sum $ concat [[zeroCounts !! (days - t2) | t2 <- dailyTimers !! (days - t)] | t <- inputTimers] 
+    --Run calculation for a single zero fish.
+    let dailyTimers = run days [[0]]
+    let dailyCounts = [length x | x <- dailyTimers]
+    let result = sum $ concat [[dailyCounts !! (days - t2) | t2 <- dailyTimers !! (days - t)] | t <- inputTimers] 
     print result
 
-run :: Int -> [[Int]] -> [Int] -> ([Int], [[Int]])
-run 0 timers counts = (counts, timers)
-run i timers counts = run (i-1) (timers ++ [next]) (counts ++ [length next])
+--Run steps keeping track of fish and 
+run :: Int -> [[Int]] -> [[Int]]
+run 0 timers = timers
+run i timers = run (i-1) (timers ++ [next])
     where 
         next = concatMap step (last timers)
 
