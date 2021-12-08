a7f52c6a0265fc39a2d6900cb1178ed67a7a0511
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-08 11:28:49 -0500

    code clean up

--- day8/day8.hs
+++ day8/day8.hs
@@ -10,29 +10,34 @@ main = do
     print $ sum $ map process dat
 
 process :: ([String], [String]) -> Int
-process (inputs, outputs) = 1000 * getInt mapping (head sortedOutput) + 100 * getInt mapping (sortedOutput !! 1) + 10 * getInt mapping (sortedOutput !! 2) + getInt mapping (last sortedOutput)
+process (inputs, outputs) = sum $ zipWith (\d n -> n * getInt mapping d) sortedOutput [1000, 100, 10, 1] 
     where 
         letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
         -- sort inputs and ouputs so we have one string per digit
         sortedInput = map L.sort inputs
         sortedOutput = map L.sort outputs
         distinct = L.nub (sortedInput ++ sortedOutput)
-        [one] = [w | w <- distinct, length w == 2]
-        [seven] = [w | w <- distinct, length w == 3]
-        [four] = [w | w <- distinct, length w == 4]
-        [eight] = [w | w <- distinct, length w == 7]
-        sixNineZero = [w | w <- distinct, length w == 6]
-        [six] = [n | n <- sixNineZero, (head one `elem` n) /= (last one `elem` n)]
-        nineZero = [n | n <- sixNineZero, n /= six]
-        [nine] = [n | n <- nineZero, head four `elem` n && four !! 1 `elem` n && four !! 2 `elem` n && four !! 3 `elem` n]
-        twoThreeFive = [w | w <- distinct, length w == 5]
-        [three] = filter (\x -> head one `elem` x && last one `elem` x) twoThreeFive
-        [zero] = filter (/= nine) nineZero
-        [top] = [c | c <- seven, c `notElem` one]
+
+        [one] = filter (\w -> length w == 2) distinct
+        [seven] = filter (\w -> length w == 3) distinct
+        [four] = filter (\w -> length w == 4) distinct
+        [eight] = filter (\w -> length w == 7) distinct
+
+        twoThreeFive = filter (\w -> length w == 5) distinct
+        sixNineZero = filter (\w -> length w == 6) distinct
+
+        [six] = filter (\n -> (head one `elem` n) /= (last one `elem` n)) sixNineZero
+        nineZero = filter (/= six) sixNineZero
+        [nine] = filter (\n -> all (`elem` n) four) sixNineZero
+        [zero] = filter (\n -> n /= nine && n /= six) sixNineZero
+
+        [three] = filter (\n -> all (`elem`n) one) twoThreeFive
+
+        [top] = filter (`notElem` one) seven
         [topRight] = filter (`notElem` six) letters
         [middle] = filter (`notElem` zero) letters
         [bottomLeft] = filter (`notElem` nine) letters
-        [bottomRight] = [c | c <- one, c `elem` six]
+        [bottomRight] = filter (`elem` six) one
         [topLeft] = filter (`notElem` three) nine
         [bottom] = filter (`notElem` [top, topLeft, topRight, middle, bottomLeft, bottomRight]) letters
         mapping = [top, topLeft, topRight, middle, bottomLeft, bottomRight, bottom]
