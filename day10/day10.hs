7499c5060ead8defce1c348de4d4ae3c2db66fce
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-10 00:38:11 -0500

    Day10 clean up

--- day10/day10.hs
+++ day10/day10.hs
@@ -8,31 +8,36 @@ import           Text.Printf
 type HeatMap = V.Vector (V.Vector Int)
 
 main = do
-    lines <- getOutput "input.txt"
-    let scores = sum $ map (`processBad` []) lines
+    lines <- lines <$> readFile "input.txt"
+    let part1 = sum $ map (`processBad` []) lines
     let goodScores = L.sort $ filter (/=0) $map (`processGood` []) lines
-    let final = goodScores !! (length goodScores `div` 2)
-    print scores
-    print goodScores
-    print final
+    let part2 = goodScores !! (length goodScores `div` 2)
+    print part1
+    print part2
 
 processBad :: [Char] -> [Char] -> Int
 processBad [] _ = 0
-processBad (c:cs) stack = if c `elem` ['(', '[', '{', '<'] 
-    then processBad cs (c:stack)
-    else if c == ')' && h == '(' || c == ']' && h == '[' || c == '}' && h == '{' || c == '>' && h == '<'
-        then processBad cs $ tail stack
-        else getBadScore c
+processBad (c:cs) stack 
+    | c `elem` ['(', '[', '{', '<'] = processBad cs (c:stack)
+    | c == ')' && h == '('
+        || c == ']' && h == '['
+        || c == '}' && h == '{'
+        || c == '>' && h == '<' 
+            = processBad cs $ tail stack
+    | otherwise = getBadScore c
     where
         h = head stack
 
 processGood :: [Char] -> [Char] -> Int
 processGood [] stack = getGoodScore stack 0
-processGood (c:cs) stack = if c `elem` ['(', '[', '{', '<'] 
-    then processGood cs (c:stack)
-    else if c == ')' && h == '(' || c == ']' && h == '[' || c == '}' && h == '{' || c == '>' && h == '<'
-        then processGood cs $ tail stack
-        else 0
+processGood (c:cs) stack
+    | c `elem` ['(', '[', '{', '<'] = processGood cs (c:stack)
+    | c == ')' && h == '('
+        || c == ']' && h == '['
+        || c == '}' && h == '{'
+        || c == '>' && h == '<' 
+            = processGood cs $ tail stack
+    | otherwise = 0
     where
         h = head stack
 
@@ -45,11 +50,10 @@ getBadScore _ = error "oops"
 
 getGoodScore :: [Char] -> Int -> Int
 getGoodScore [] s = s
-getGoodScore ('(':cs) s = getGoodScore cs ((5 * s) + 1)
-getGoodScore ('[':cs) s = getGoodScore cs ((5 * s) + 2)
-getGoodScore ('{':cs) s = getGoodScore cs ((5 * s) + 3)
-getGoodScore ('<':cs) s = getGoodScore cs ((5 * s) + 4)
-getGoodScore _ _ = error "oops"
-
-getOutput :: String -> IO [String]
-getOutput path = lines <$> readFile path
+getGoodScore (c:cs) s = getGoodScore cs ((5 * s) + getCharScore c)
+    where 
+        getCharScore '(' = 1
+        getCharScore '[' = 2
+        getCharScore '{' = 3
+        getCharScore '<' = 4
+        getCharScore _ = error "oops"
