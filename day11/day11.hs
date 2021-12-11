e35b2283bf08d970d50a6cc68ec7056129cb4bff
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-11 01:14:23 -0500

    cleanup 11

--- day11/day11.hs
+++ day11/day11.hs
@@ -6,44 +6,47 @@ type FlashMap = V.Vector (V.Vector Int)
 
 main = do
     flashMap <- getOutput "input.txt"
-    print flashMap
-    print $ step flashMap 100 0
+    print $ step flashMap 100
     print $ findAllFlash flashMap 1
 
-step :: FlashMap -> Int -> Int -> (FlashMap, Int)
-step f 0 c = (f, c)
-step f s c = step f'' (s-1) (c + c')
+step :: FlashMap -> Int -> Int
+step _ 0 = 0
+step f s = c + step f'' (s-1)
     where 
         fNext = increaseAll f 0 0
-        (f', c') = processFlashes fNext []
+        (f', c) = processFlashes fNext []
         f'' = resetFlashes f' 0 0
 
 findAllFlash :: FlashMap -> Int -> Int
-findAllFlash f s = if c' == 100 then s else findAllFlash f'' (s+1)
+findAllFlash f s 
+    | c' == 100 = s 
+    | otherwise = findAllFlash f'' (s+1)
     where 
         fNext = increaseAll f 0 0
         (f', c') = processFlashes fNext []
         f'' = resetFlashes f' 0 0
 
 processFlashes :: FlashMap -> [(Int, Int)] -> (FlashMap, Int)
-processFlashes f s = if length flashes == length flashes' then (f', length flashes) else processFlashes f' flashes
+processFlashes f s 
+    | length flashes == length flashes' = (f', length flashes)
+    | otherwise = processFlashes f' flashes
     where 
-        flashes = getFlashes f 0 0 []
+        flashes = getFlashes f 0 0
         f' = executeFlashes f (filter (`notElem` s) flashes)
-        flashes' = getFlashes f' 0 0 []
+        flashes' = getFlashes f' 0 0
 
 executeFlashes :: FlashMap -> [(Int,Int)] -> FlashMap
 executeFlashes f [] = f
 executeFlashes f ((x,y):bs) = executeFlashes (increaseAdjacent f x y) bs
 
-getFlashes :: FlashMap -> Int -> Int -> [(Int,Int)] -> [(Int, Int)]
-getFlashes f x y b 
-    | y == V.length f = b
-    | x == V.length f = getFlashes f 0 (y + 1) b
-    | otherwise = getFlashes f (x+1) y b'
+getFlashes :: FlashMap -> Int -> Int  -> [(Int, Int)]
+getFlashes f x y 
+    | y == V.length f = []
+    | x == V.length f = getFlashes f 0 (y + 1)
+    | otherwise = flash ++ getFlashes f (x+1) y
     where 
         value = (f V.! y) V.! x 
-        b' = if value == 10 then (x,y) : b else b
+        flash = [(x,y) | value == 10]
 
 resetFlashes :: FlashMap -> Int -> Int -> FlashMap
 resetFlashes f x y 
@@ -52,7 +55,9 @@ resetFlashes f x y
     | otherwise = resetFlashes f' (x+1) y
     where 
         value = (f V.! y) V.! x 
-        f' = if value == 10 then f V.// [(y, f V.! y V.// [(x, 0)])] else f
+        f' 
+            | value == 10 = f V.// [(y, f V.! y V.// [(x, 0)])]
+            | otherwise = f
 
 increaseAll :: FlashMap -> Int -> Int -> FlashMap
 increaseAll f x y 
@@ -73,7 +78,6 @@ increaseAdjacent f x y = f''''''''
         f'''''' = increase f''''' (x-1) (y+1)
         f''''''' = increase f'''''' x (y+1)
         f'''''''' = increase f''''''' (x+1) (y+1)
-
 
 increase :: FlashMap -> Int -> Int -> FlashMap
 increase f x y 
