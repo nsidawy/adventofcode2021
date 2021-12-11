eab7c15be9def5f5d6d16fec603bba622eebe9f2
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-11 01:49:33 -0500

    day11 clean up a lot

--- day11/day11.hs
+++ day11/day11.hs
@@ -1,11 +1,12 @@
 import           Control.Monad
+import           Control.Monad.State
 import           Data.List.Split
 import qualified Data.Vector     as V
 
 type FlashMap = V.Vector (V.Vector Int)
 
 main = do
-    flashMap <- getOutput "input.txt"
+    flashMap <- getInput "input.txt"
     print $ step flashMap 100
     print $ findAllFlash flashMap 1
 
@@ -13,71 +14,56 @@ step :: FlashMap -> Int -> Int
 step _ 0 = 0
 step f s = c + step f'' (s-1)
     where 
-        fNext = increaseAll f 0 0
+        fNext = increaseAll f
         (f', c) = processFlashes fNext []
-        f'' = resetFlashes f' 0 0
+        f'' = resetFlashes f'
 
 findAllFlash :: FlashMap -> Int -> Int
 findAllFlash f s 
     | c' == 100 = s 
     | otherwise = findAllFlash f'' (s+1)
     where 
-        fNext = increaseAll f 0 0
+        fNext = increaseAll f
         (f', c') = processFlashes fNext []
-        f'' = resetFlashes f' 0 0
+        f'' = resetFlashes f'
 
 processFlashes :: FlashMap -> [(Int, Int)] -> (FlashMap, Int)
 processFlashes f s 
     | length flashes == length flashes' = (f', length flashes)
     | otherwise = processFlashes f' flashes
     where 
-        flashes = getFlashes f 0 0
+        flashes = getFlashes f
         f' = executeFlashes f (filter (`notElem` s) flashes)
-        flashes' = getFlashes f' 0 0
+        flashes' = getFlashes f'
 
 executeFlashes :: FlashMap -> [(Int,Int)] -> FlashMap
 executeFlashes f [] = f
 executeFlashes f ((x,y):bs) = executeFlashes (increaseAdjacent f x y) bs
 
-getFlashes :: FlashMap -> Int -> Int  -> [(Int, Int)]
-getFlashes f x y 
-    | y == V.length f = []
-    | x == V.length f = getFlashes f 0 (y + 1)
-    | otherwise = flash ++ getFlashes f (x+1) y
-    where 
-        value = (f V.! y) V.! x 
-        flash = [(x,y) | value == 10]
+getFlashes :: FlashMap  -> [(Int, Int)]
+getFlashes f = [(x, y) 
+        | x <- [0..d]
+        , y <- [0..d]
+        , (f V.! y) V.! x == 10]
+    where
+        d = V.length f - 1
 
-resetFlashes :: FlashMap -> Int -> Int -> FlashMap
-resetFlashes f x y 
-    | y == V.length f = f
-    | x == V.length f = resetFlashes f 0 (y + 1)
-    | otherwise = resetFlashes f' (x+1) y
-    where 
-        value = (f V.! y) V.! x 
-        f' 
-            | value == 10 = f V.// [(y, f V.! y V.// [(x, 0)])]
-            | otherwise = f
+resetFlashes :: FlashMap -> FlashMap
+resetFlashes = V.map (V.map (\x -> if x == 10 then 0 else x))
 
-increaseAll :: FlashMap -> Int -> Int -> FlashMap
-increaseAll f x y 
-    | y == V.length f = f
-    | x == V.length f = increaseAll f 0 (y + 1)
-    | otherwise = increaseAll f' (x+1) y
-    where 
-        f' = increase f x y
+increaseAll :: FlashMap -> FlashMap
+increaseAll = V.map (V.map (\x -> if x == 10 then 10 else x + 1))
 
 increaseAdjacent :: FlashMap -> Int -> Int -> FlashMap
-increaseAdjacent f x y = f''''''''
-    where
-        f' = increase f (x-1) (y-1)
-        f'' = increase f' x (y-1)
-        f''' = increase f'' (x+1) (y-1)
-        f'''' = increase f''' (x-1) y
-        f''''' = increase f'''' (x+1) y
-        f'''''' = increase f''''' (x-1) (y+1)
-        f''''''' = increase f'''''' x (y+1)
-        f'''''''' = increase f''''''' (x+1) (y+1)
+increaseAdjacent f x y = flip execState f $ do
+    modify $ \f' -> increase f' (x-1) (y-1)
+    modify $ \f' -> increase f' x (y-1)
+    modify $ \f' -> increase f' (x+1) (y-1)
+    modify $ \f' -> increase f' (x-1) y
+    modify $ \f' -> increase f' (x+1) y
+    modify $ \f' -> increase f' (x-1) (y+1)
+    modify $ \f' -> increase f' x (y+1)
+    modify $ \f' -> increase f' (x+1) (y+1)
 
 increase :: FlashMap -> Int -> Int -> FlashMap
 increase f x y 
@@ -87,8 +73,8 @@ increase f x y
         value = (f V.! y) V.! x 
         newValue = if value == 10 then 10 else value + 1
 
-getOutput :: String -> IO FlashMap
-getOutput path = do
+getInput :: String -> IO FlashMap
+getInput path = do
     lines <- lines <$> readFile path
     return $ V.fromList $ map parseLine lines
     where 
