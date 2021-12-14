5fc45fcc25f6ff31130a90bf2a1205f353f5805d
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-14 05:26:18 -0500

    done day14 i guess

--- day14/day14.hs
+++ day14/day14.hs
@@ -8,21 +8,20 @@ import           Text.Printf
 
 main = do
     (start, transforms) <- getInput "input.txt"
-    print (start, transforms)
     let part1 = runSteps start transforms 10
     print $ getCounts part1
 
     let countsByPair = M.fromList $ map (`getCountsByPair` transforms) $ M.keys transforms
-    print "got counts by pair"
     let start' = V.fromList $ runSteps start transforms 20
     let pairs = [[start' V.! i, start' V.! (i+1)] | i <- [0..V.length start' -2]] 
     let pairCounts = [fromJust $ M.lookup p countsByPair | p <- pairs]
-    print $ V.length start'
-    print $ length pairs
-    print $ length pairCounts
-    --let counts =  getCountsFromAggs start' countsByPair M.empty 0
-    let counts = foldl (\s c -> M.unionWith (+) s c) M.empty pairCounts
-    let counts' = M.insertWith (+) (V.last start') 1 counts
+    --let counts = foldl (\s c -> M.unionWith (+) s c) M.empty pairCounts
+    let counts = foldl (\s c -> M.unionWith (+) s c) M.empty (take 4000000 pairCounts)
+    let counts' = foldl (\s c -> M.unionWith (+) s c) counts (take 4000000 $ drop 4000000  pairCounts)
+    let counts'' = foldl (\s c -> M.unionWith (+) s c) counts' (take 4000000 $ drop 8000000  pairCounts)
+    let counts''' = foldl (\s c -> M.unionWith (+) s c) counts'' (take 4000000 $ drop 12000000  pairCounts)
+    let counts'''' = foldl (\s c -> M.unionWith (+) s c) counts''' (take 4000000 $ drop 16000000  pairCounts)
+    let counts''''' = M.insertWith (+) (V.last start') 1 counts''''
     print counts'
     let min = minimum [c | (_, c) <- M.toList counts']
     let max = maximum [c | (_, c) <- M.toList counts']
@@ -32,17 +31,6 @@ getCountsByPair :: String -> M.Map String Char -> (String, M.Map Char Int)
 getCountsByPair s m = (s, M.fromListWith (+) (zip (take (length step - 1) step) (repeat 1)))
     where
         step = runSteps s m 20
-
-getCountsFromAggs :: V.Vector Char -> M.Map String (M.Map Char Int) -> M.Map Char Int -> Int -> M.Map Char Int
-getCountsFromAggs cs m m0 i
-    | i == V.length cs - 1 = m0
-    | [c1,c2] `M.member` m = getCountsFromAggs cs m m0' (i+1)
-    | otherwise = error "uh oh" -- c1 : step (c2:cs) m
-    where
-        counts = fromJust $ [c1,c2] `M.lookup` m
-        c1 = cs V.! i
-        c2 = cs V.! (i+1)
-        m0' = foldl (\s (ch, c) -> M.insertWith (+) ch c s) m0 $ M.toList counts
 
 getCounts :: String -> Int
 getCounts s = max - min
