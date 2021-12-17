0826416bdaf956f3a7b18ca252c7f965a84a5eef
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-17 16:13:46 -0500

    minor clean up

--- day17/day17.hs
+++ day17/day17.hs
@@ -12,8 +12,8 @@ main = do
 
     let countsByX = [(x, validXCounts) | x <- xs, let validXCounts = take takeSize $ isXValid x 0 minX maxX 0, not $ null validXCounts]
     let allXCounts = concat [[(x,c) | c <- cs] | (x, cs) <- countsByX]
-    let validVelocities = L.sort . L.nub $ [(x,y,c) | (x,c) <- allXCounts, y <- yPoss, isYValid y 0 minY maxY c]
-    let result = maximum . concat $ [getYs y 0 c | (_,y,c) <- validVelocities]
+    let validVelocities = L.sort . L.nub $ [(x,y,c) | (x,c) <- allXCounts, y <- yPoss, isYValid y minY maxY c]
+    let result = maximum $ [(y * (y + 1)) `div` 2 | (_,y,c) <- validVelocities]
     print result
 
     print $ length . L.nub $ [(x,y) | (x,y,_) <- validVelocities]
@@ -29,12 +29,11 @@ isXValid x curX minX maxX cnt
         x' = if x /= 0 then x - 1 else x
         rest = isXValid x' (curX + x) minX maxX (cnt+1)
 
-isYValid :: Int -> Int -> Int -> Int -> Int -> Bool
-isYValid y curY minY maxY cnt 
-    | curY < minY = False
-    | curY >= minY && curY <= maxY && cnt == 0 = True
-    | otherwise = isYValid (y-1) (curY + y) minY maxY (cnt-1)
+isYValid :: Int -> Int -> Int -> Int -> Bool
+isYValid y minY maxY cnt = final >= minY && final <= maxY
+    where 
+        final = getFinalY y 0 cnt
 
-getYs :: Int -> Int -> Int -> [Int]
-getYs y cur 0 = [cur] 
-getYs y cur c = cur : getYs (y-1) (cur + y) (c-1)
+getFinalY :: Int -> Int -> Int -> Int
+getFinalY y cur 0 = cur 
+getFinalY y cur c = getFinalY (y-1) (cur + y) (c-1)
