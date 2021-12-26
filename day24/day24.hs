f5ad4bcfcc460f90f3e8253cc58b2fd4467109b0
Nick Sidawy <nick.sidawy@gmail.com>
2021-12-25 22:03:41 -0500

    slightly faster d24

--- day24/day24.hs
+++ day24/day24.hs
@@ -6,129 +6,117 @@ import qualified Data.Map            as M
 import qualified Data.Vector            as V
 import           Data.Maybe
 import qualified Text.Read           as R
+import System.Environment
 
 data Var =  W | X | Y | Z
     deriving (Show, Ord, Eq)
-data Value = V Var | I Int | In Instruction | Input Int
+data Value = V Var | I Int
     deriving (Show)
 data Instruction =
-    Inp Value
-    | Add Value Value
-    | Mul Value Value
-    | Div Value Value
-    | Mod Value Value
-    | Eql Value Value
+    Inp Var
+    | Add Var Value
+    | Mul Var Value
+    | Div Var Value
+    | Mod Var Value
+    | Eql Var Value
     deriving (Show)
 
-type Register = M.Map Var Int
+type Register = (Int,Int,Int,Int)
 type ARegister = M.Map Var Value
 
-defaultRegister = M.fromList [(X,0), (Y,0), (Z,0), (W,0)]
+defaultRegister = (0,0,0,0)
 defaultARegister = M.fromList [(X,I 0), (Y,I 0), (Z,I 0), (W,I 0)]
 
 main = do
     instructions <- getInput "input.txt"
-    let maxMonad = 99999936421899
-    --let maxMonad = 51111114521335
+    start <- getArgs
+    let maxMonad = (read (head start)) * 1000000000000 + 999936421899
     --print instructions
-    let collapsed = collapse instructions defaultARegister 0
-    let evaluated = [(a, evaluate v $ V.fromList [7]) | (a,v) <- M.toList collapsed]
-    --print $ collapsed M.! Z
-    --print evaluated
-    print <$> findMaxMonad maxMonad (collapsed M.! Z)
+    let ci = chunkByInput instructions
+    --print <$> runChunk ci defaultRegister 9 []
+    print <$> findMaxMonad maxMonad instructions
 
-findMaxMonad :: Int -> Value -> IO Int
-findMaxMonad monad v
-    | 0 `elem` monadInts = findMaxMonad (monad - 1) v
+chunkByInput :: [Instruction] -> [[Instruction]]
+chunkByInput [] = []
+chunkByInput is = (head is : takeWhile isNotInput (tail is)) : chunkByInput (dropWhile (\i -> isNotInput i) (tail is))
+    where
+        isNotInput (Inp _) = False
+        isNotInput _ = True
+
+findMaxMonad :: Int -> [Instruction] -> IO Int
+findMaxMonad monad is
+    | 0 `elem` monadInts = findMaxMonad (monad - 1) is
     | result == 0 = return monad
     | otherwise = do
-        print $ show monad ++ "\t" ++ show result
-        findMaxMonad (monad - 1) v
+        when (drop 9 monadInts == [9,9,9,9,9]) $ print $ show monad ++ "\t" ++ show result
+        findMaxMonad (monad - 1) is
     where
         register = M.fromList [(X,0), (Y,0), (Z,0), (W,0)]
-        monadInts = V.fromList $ map C.digitToInt $ show monad
-        result = evaluate v monadInts
+        monadInts = map C.digitToInt $ show monad
+        (_,_,_,result) = run is defaultRegister monadInts
 
-collapse :: [Instruction] -> ARegister -> Int -> ARegister
-collapse [] r n = r
-collapse (i:is) r n = case i of
+runChunk :: [[Instruction]] -> Register -> Int -> [Int] -> IO (Maybe [Int])
+runChunk [] (_,_,_,z) _ e
+    | z == 0 = return $ Just e
+    | otherwise = do
+        when (take 5 e == [9,9,9,9,9]) $ print (e,z) 
+        return Nothing 
+runChunk (i:is) r n e
+    | n == 0 = return Nothing
+    | otherwise = do
+        answer <- L.find isJust <$> mapM (\x -> runChunk is r' x (n:e)) tries
+        return $ fromMaybe Nothing answer
+    where 
+        r' = run i r [n]
+        tries = [9,8,7,6,5,4,3,2,1]
+        
+
+run :: [Instruction] -> Register -> [Int] -> Register
+run [] r _ = r
+run (i:is) r inp = case i of
     Inp var -> let
-        v' = Input n
-        var' = case var of
-            V a -> a
-            _   -> error "bad input"
-        r' = M.insert var' v' r
-        in collapse is r' (n+1)
-    Add (V var) val -> let
-        aValue = r M.! var
-        bValue = case val of
-            V a -> r M.! a
-            _   -> val
-        v' = case (aValue, bValue) of
-            (I 0, _)   -> bValue
-            (_, I 0)   -> aValue
-            (I a, I b) -> I (a+b)
-            _          -> In (Add aValue bValue)
-        r' = M.insert var v' r
-        in collapse is r' n
-    Mul (V var) val -> let
-        aValue = r M.! var
-        bValue = case val of
-            V a -> r M.! a
-            _   -> val
-        v' = case (aValue, bValue) of
-            (I 0, _)   -> I 0
-            (_, I 0)   -> I 0
-            (I 1, _)   -> bValue
-            (_, I 1)   -> aValue
-            (I a, I b) -> I (a*b)
-            _          -> In (Mul aValue bValue)
-        r' = M.insert var v' r
-        in collapse is r' n
-    Div (V var) val -> let
-        aValue = r M.! var
-        bValue = case val of
-            V a -> r M.! a
-            _   -> val
-        v' = case (aValue, bValue) of
-            (_, I 1)   -> aValue
-            (I a, I b) -> I (a `div` b)
-            _          -> In (Div aValue bValue)
-        r' = M.insert var v' r
-        in collapse is r' n
-    Mod (V var) val -> let
-        aValue = r M.! var
-        bValue = case val of
-            V a -> r M.! a
-            _   -> val
-        v' = case (aValue, bValue) of
-            (_, I 1)   -> I 0
-            (I a, I b) -> I (a `mod` b)
-            _          -> In (Mod aValue bValue)
-        r' = M.insert var v' r
-        in collapse is r' n
-    Eql (V var) val -> let
-        aValue = r M.! var
-        bValue = case val of
-            V a -> r M.! a
-            _   -> val
-        v' = case (aValue, bValue) of
-            (I a, I b)     -> I (if a == b then 1 else 0)
-            (Input a, I b) -> if b > 9 then I 0 else In (Eql (Input a) (I b))
-            (I a, Input b) -> if a > 9 then I 0 else In (Eql (I a) (Input b))
-            _              -> In (Eql aValue bValue)
-        r' = M.insert var v' r
-        in collapse is r' n
+        v' = head inp
+        r' = setRegister r var v'
+        in run is r' $ tail inp
+    Add var val -> let
+        v' = getVar var + getValue val
+        r' = setRegister r var v'
+        in run is r' inp
+    Mul var val -> let
+        v' = getVar var * getValue val
+        r' = setRegister r var v'
+        in run is r' inp
+    Div var val -> let
+        v' = getVar var `div` getValue val
+        r' = setRegister r var v'
+        in run is r' inp
+    Mod var val -> let
+        v' = getVar var `mod` getValue val
+        r' = setRegister r var v'
+        in run is r' inp
+    Eql var val -> let
+        v' = if getVar var == getValue val then 1 else 0
+        r' = setRegister r var v'
+        in run is r' inp
+    where
+        getValue v = case v of
+            I x -> x
+            V x -> getVar x
+        getVar v = getRegister r v
 
-evaluate :: Value -> V.Vector Int -> Int
-evaluate (I i) is            = i
-evaluate (Input i) is        = is V.! i
-evaluate (In (Add v1 v2)) is = evaluate v1 is + evaluate v2 is
-evaluate (In (Mul v1 v2)) is = evaluate v1 is * evaluate v2 is
-evaluate (In (Div v1 v2)) is = evaluate v1 is `div` evaluate v2 is
-evaluate (In (Mod v1 v2)) is = evaluate v1 is `mod` evaluate v2 is
-evaluate (In (Eql v1 v2)) is = if evaluate v1 is == evaluate v2 is then 1 else 0
-evaluate x is                = error$ show x
+getRegister :: Register -> Var -> Int
+getRegister (w,x,y,z) v 
+    | v == W = w
+    | v == X = x
+    | v == Y = y
+    | v == Z = z
+
+setRegister :: Register -> Var -> Int -> Register
+setRegister (w,x,y,z) v i
+    | v == W = (i,x,y,z) 
+    | v == X = (w,i,y,z) 
+    | v == Y = (w,x,i,z) 
+    | v == Z = (w,x,y,i) 
 
 getInput :: String -> IO [Instruction]
 getInput path = do
@@ -147,45 +135,12 @@ parseLine s
         (ins:rest) = words s
 
         parseVar s
-            | s == "x" = V X
-            | s == "y" = V Y
-            | s == "z" = V Z
-            | s == "w" = V W
+            | s == "x" = X
+            | s == "y" = Y
+            | s == "z" = Z
+            | s == "w" = W
 
         parseValue :: String -> Value
         parseValue s
             | isJust (R.readMaybe s :: Maybe Int) = I (read s :: Int)
-            | otherwise = parseVar s
-
---run :: [Instruction] -> Register -> [Int] -> Register
---run [] r _ = r
---run (i:is) r inp = case i of
---    Inp var -> let
---        v' = head inp
---        r' = M.insert var v' r
---        in run is r' $ tail inp
---    Add var val -> let
---        v' = getVar var + getValue val
---        r' = M.insert var v' r
---        in run is r' inp
---    Mul var val -> let
---        v' = getVar var * getValue val
---        r' = M.insert var v' r
---        in run is r' inp
---    Div var val -> let
---        v' = getVar var `div` getValue val
---        r' = M.insert var v' r
---        in run is r' inp
---    Mod var val -> let
---        v' = getVar var `mod` getValue val
---        r' = M.insert var v' r
---        in run is r' inp
---    Eql var val -> let
---        v' = if getVar var == getValue val then 1 else 0
---        r' = M.insert var v' r
---        in run is r' inp
---    where
---        getValue v = case v of
---            I x -> x
---            V x -> getVar x
---        getVar v = r M.! v
+            | otherwise = V (parseVar s)
